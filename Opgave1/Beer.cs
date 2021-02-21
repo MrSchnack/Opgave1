7d3838bbe37fb49be15e01c4dd274e8767a665b4
MrSchnack <philip.s.petersen@gmail.com>
2021-02-21 17:11:19 +0100

    Add project files.

---
+++ Opgave1/Beer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Opgave1
+{
+
+    class Beer
+    {
+        private string name;
+        private double alcoholamount;
+        private double price;
+
+        public int Id { get; set; }
+        public string Name { get => name; set
+            {
+                if (value.Length < 4) throw new ArgumentException("Name has to be longer than 4 characters"); }
+            }
+        public double Price{ get => price; set
+            {
+                if (value < 0) throw new ArgumentOutOfRangeException("Price cannot be 0");
+            }
+        }
+        public double Alcoholamount { get => alcoholamount; set => alcoholamount = value; }
+
+
+
+    }
+
+}
